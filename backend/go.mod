5fb92db0a6274cfceec140664fdf11799dd69bcb
Michał Łepik <michal.lepik@hotmail.com>
2020-12-05 19:08:54 +0100

    Add docker config for backend

--- backend/go.mod
+++ backend/go.mod
@@ -5,6 +5,12 @@ go 1.15
 require (
 	github.com/andybalholm/brotli v1.0.1 // indirect
 	github.com/gofiber/fiber/v2 v2.2.3
+	github.com/jackc/pgx/v4 v4.10.0 // indirect
 	github.com/klauspost/compress v1.11.3 // indirect
+	golang.org/x/crypto v0.0.0-20201203163018-be400aefbc4c // indirect
 	golang.org/x/sys v0.0.0-20201204225414-ed752295db88 // indirect
+	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gorm.io/driver/postgres v1.0.5
+	gorm.io/gorm v1.20.8
 )
--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,35 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"os"
+	"punktomat/database"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func initDatabase() {
+	var err error
+	dsn := fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%s sslmode=disable",
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"))
+
+	database.DBConn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Database connection successful")
+}
 
 func main() {
 	app := fiber.New()
+	initDatabase()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("test")
