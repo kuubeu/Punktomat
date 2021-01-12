cff31a6b84b4ddc14fb26bc58f53fa7cb0c03d3b
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-01-12 21:03:40 +0100

    Add table with multi select

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,43 +1,45 @@
-<template
-  ><div class="page-container">
-    <md-app md-mode="fixed">
-      <md-app-toolbar class="md-primary">
-        <span class="md-title">Punktomat</span>
-      </md-app-toolbar>
-      <md-app-content>
-        <md-table v-model="info" md-card>
-          <md-table-toolbar>
-            <h1 class="md-title">Lista czasopism naukowych</h1>
-          </md-table-toolbar>
-
-          <md-table-row slot="md-table-row" slot-scope="{ item }">
-            <md-table-cell md-label="ID" md-sort-by="ID">{{
-              item.ID
-            }}</md-table-cell>
-            <md-table-cell md-label="Punkty" md-sort-by="points">{{
-              item.points
-            }}</md-table-cell>
-            <md-table-cell md-label="Tytuł" md-sort-by="title">{{
-              item.title
-            }}</md-table-cell>
-            <md-table-cell md-label="ISSN" md-sort-by="issn">{{
-              item.issn
-            }}</md-table-cell>
-            <md-table-cell md-label="Kategorie">
-              <div class="tags">
-                <div
-                  class="chip"
-                  v-bind:key="item.issn + category"
-                  v-for="category in item.Categories"
-                >
-                  {{ category }}
-                </div>
+<template>
+  <div class=".container-fluid">
+    <b-navbar variant="faded" type="light">
+      <b-navbar-brand tag="h1" class="mb-0">Punktomat</b-navbar-brand>
+    </b-navbar>
+    <div class="row justify-content-md-center">
+      <div class="col-11">
+        <b-table
+          :fields="fields"
+          :items="magazines"
+          :select-mode="selectMode"
+          ref="selectableTable"
+          selectable
+          @row-selected="onRowSelected"
+        >
+          <template #cell(ID)="data">
+            {{ data.item.ID }}
+          </template>
+          <template #cell(points)="data">
+            {{ data.item.points }}
+          </template>
+          <template #cell(title)="data">
+            {{ data.item.title }}
+          </template>
+          <template #cell(issn)="data">
+            {{ data.item.issn }}
+          </template>
+          <template #cell(Categories)="data">
+            <div class="tags">
+              <div
+                class="chip"
+                v-for="category in data.item.Categories"
+                v-bind:key="data.item.issn + category"
+              >
+                {{ category }}
               </div>
-            </md-table-cell>
-          </md-table-row>
-        </md-table>
-      </md-app-content>
-    </md-app>
+            </div>
+          </template>
+        </b-table>
+      </div>
+    </div>
+    {{ selected }}
   </div>
 </template>
 
@@ -48,70 +50,34 @@ export default {
   name: "Main",
   data() {
     return {
-      info: [
-        {
-          ID: 1,
-          title: "2D Materials",
-          issn: "2053-1583",
-          "e-issn": "2053-1583",
-          secondTitle: "2D Materials",
-          secondIssn: "",
-          "secondE-issn": "2053-1583",
-          points: 140,
-          Categories: [
-            "architektura i urbanistyka",
-            "automatyka, elektronika i elektrotechnika",
-            "inżynieria biomedyczna",
-            "inżynieria lądowa i transport",
-            "inżynieria materiałowa",
-            "inżynieria mechaniczna",
-            "nauki farmaceutyczne",
-            "nauki chemiczne",
-            "nauki fizyczne",
-          ],
-        },
-        {
-          ID: 2,
-          title: "3 Biotech",
-          issn: "2190-572X",
-          "e-issn": "2190-5738",
-          secondTitle: "3 Biotech",
-          secondIssn: "2190-572X",
-          "secondE-issn": "2190-5738",
-          points: 70,
-          Categories: [
-            "architektura i urbanistyka",
-            "inżynieria biomedyczna",
-            "inżynieria chemiczna",
-            "inżynieria środowiska, górnictwo i energetyka",
-            "nauki farmaceutyczne",
-            "nauki o zdrowiu",
-            "rolnictwo i ogrodnictwo",
-            "zootechnika i rybactwo",
-            "nauki biologiczne",
-          ],
-        },
-        {
-          ID: 3,
-          title: "3C Empresa",
-          issn: "2254-3376",
-          "e-issn": "2254-3376",
-          secondTitle: "",
-          secondIssn: "",
-          "secondE-issn": "",
-          points: 20,
-          Categories: [
-            "architektura i urbanistyka",
-            "nauki o zarządzaniu i jakości",
-          ],
-        },
+      magazines: null,
+      loading: true,
+      fields: [
+        "ID",
+        "title",
+        "issn",
+        "points",
+        { key: "Categories", label: "Kategorie" },
       ],
+      selectMode: "multi",
+      numbers: [...Array(20).keys()],
+      selected: [],
     };
   },
+  methods: {
+    onRowSelected(items) {
+      this.selected = items;
+    },
+  },
   mounted() {
-    axios.get("http://127.0.0.1:4000").then((response) => {
-      this.info = response.data;
-    });
+    setTimeout(
+      () =>
+        axios.get("http://127.0.0.1:4000").then((response) => {
+          this.magazines = response.data;
+          this.loading = false;
+        }),
+      2000
+    );
   },
 };
 </script>
