d8907a8c1133d9cdbd771a6f6d8ed02c3e8b85b8
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-01-12 21:03:10 +0100

    Change vueMaterialUI to vueBootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,12 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
+import PortalVue from "portal-vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-import VueMaterial from "vue-material";
-import "vue-material/dist/vue-material.min.css";
-import "vue-material/dist/theme/default.css";
+//css files
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(VueMaterial);
+Vue.use(PortalVue);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   render: (h) => h(App),
