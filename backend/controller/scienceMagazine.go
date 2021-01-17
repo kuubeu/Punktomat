3954c4fd8040dc4bec2799f0f253d877c80a12e7
Michał Łepik <michal.lepik@hotmail.com>
2021-01-17 13:15:31 +0100

    Add pagination

--- backend/controller/scienceMagazine.go
+++ backend/controller/scienceMagazine.go
@@ -3,14 +3,28 @@ package controller
 import (
 	"punktomat/database"
 	"punktomat/model"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-// GetScienceMagazines returns all sience magazines
+// GetScienceMagazines returns all science magazines
 func GetScienceMagazines(c *fiber.Ctx) error {
 	db := database.DBConn
 	var scienceMagazine []model.ScienceMagazine
-	db.Find(&scienceMagazine)
-	return c.JSON(scienceMagazine)
+	var count int64
+
+	limit, err := strconv.Atoi(c.Query("limit", "100"))
+	offset, err := strconv.Atoi(c.Query("offset", "0"))
+
+	if err != nil {
+		return c.Status(400).SendString("Invalid query param")
+	}
+
+	db.Model(&model.ScienceMagazine{}).Count(&count)
+	db.Offset(offset).Limit(limit).Find(&scienceMagazine)
+
+	return c.JSON(fiber.Map{
+		"total": count, "results": scienceMagazine,
+	})
 }
