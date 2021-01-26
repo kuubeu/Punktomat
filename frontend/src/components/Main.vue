936b4d6d42b1247bfffbe2fe12da6ec3d7664b17
kuubeu <kuubeu@gmail.com>
2021-01-26 15:45:57 +0100

    Replace icon

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -152,7 +152,7 @@
                 genPDF();
               "
             >
-              <v-icon left>mdi-tray-arrow-down</v-icon> Zapisz do PDF
+              <v-icon left>mdi-download-outline</v-icon> Zapisz do PDF
             </v-btn>
           </v-card-actions>
         </v-card>
