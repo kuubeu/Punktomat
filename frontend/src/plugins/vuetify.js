b20d048e66d2b23cfe3f54d72cdcc0fb8638abdc
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-01-27 13:52:57 +0100

    Translation to polish

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,11 +1,15 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-
-import colors from 'vuetify/lib/util/colors';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import pl from "vuetify/es5/locale/pl";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  lang: {
+    locales: { pl },
+    current: "pl",
+  },
   theme: {
     themes: {
       light: {
